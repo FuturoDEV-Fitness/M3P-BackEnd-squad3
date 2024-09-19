9b7ef832761ee07230d887b70e16b3f51e695329
Thaís <thaisefag@gmail.com>
2024-09-18 22:23:42 -0300

    :package:dendencias

--- src/server.js
+++ src/server.js
@@ -2,16 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const routes = require("./routes/routes");
 const connection = require("./database/connection");
+const APP_PORT = process.env.APP_PORT || 3000;
+console.log(APP_PORT);
 
 require("dotenv").config();
 
 class Server {
   constructor() {
     this.server = express();
-    this.middlewares();
+    this.middlewares(server);
     this.database();
     this.routes();
-    this.initializeServer();
+    this.initializeServer(server);
   }
 
   async middlewares() {
@@ -26,8 +28,8 @@ class Server {
       console.log("Conectando ao banco de dados");
       await connection.authenticate();
       console.log("Conectado ao banco de dados");
-    } catch {
-      console.log("Erro ao conectar ao banco de dados");
+    } catch (error) {
+      console.log("Erro ao conectar ao banco de dados", error);
     }
   }
 
@@ -37,9 +39,9 @@ class Server {
     console.log("Rotas executadas");
   }
 
-  async initializeServer() {
-    this.server.listen(process.env.APP_PORT, () => {
-      console.log(`Servidor inicializado na porta ${process.env.APP_PORT}`);
+  async initializeServer(server) {
+    server.listen(APP_PORT, () => {
+      console.log(`Servidor rodando na porte: ${APP_PORT}`);
     });
   }
 }
