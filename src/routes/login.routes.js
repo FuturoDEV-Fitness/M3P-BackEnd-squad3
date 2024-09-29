d1bcda29b9426767a98e3340e043ea1e33ff9101
Thaís <thaisefag@gmail.com>
2024-09-29 14:19:16 -0300

    manutenção de controller

--- src/routes/login.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const LoginController = require("../controllers/LoginController");
-
-const loginRoutes = new Router();
-
-loginRoutes.post("/login", LoginController.login);
-
-module.exports = loginRoutes;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,13 +3,14 @@ const { Router } = require("express");
 const SwaggerUI = requeire("swagger-ui-express");
 const swaggerDocument = require("/doc.swagger.json");
 
-const usuariosRoutes = require("./usuarios.routes.js");
-const loginRoutes = require("./login.routes.js");
+const usuarioRoutes = require("./usuarios.routes.js");
+const autenticUsuarioRoutes = require("./autentic.usuario.routes.js");
 const locaisRoutes = require("./locais.routes.js");
 
 const router = Router();
 
-router.use("/usuarios", usuariosRoutes);
-router.use("/login", loginRoutes);
+router.use("/usuarios", usuarioRoutes);
+router.use("/", autenticUsuarioRoutes);
 router.use("/locais", locaisRoutes);
+
 module.exports = router;
