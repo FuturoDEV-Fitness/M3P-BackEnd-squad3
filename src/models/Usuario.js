ec8306daac84a605b5d6cff2c406bcd02c755cbd
Thaís <thaisefag@gmail.com>
2024-10-01 21:51:17 -0300

    :sparkles:

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -2,6 +2,7 @@ const { DataTypes } = require("sequelize");
 const connection = require("../database/connection");
 const { hashSync } = require("bcryptjs");
 const Local = require("./Local");
+const Avaliacao = require("./Avaliacao");
 
 const Usuario = connection.define("usuarios", {
   id: {
@@ -46,15 +47,14 @@ const Usuario = connection.define("usuarios", {
   },
 });
 
-// Usuario.belongsToMany(Local, {
-//   through:
-// })
-
 Usuario.beforeSave((usuario) => {
   if (usuario.password_hash) {
     usuario.password_hash = hashSync(usuario.password_hash, 10);
   }
   return usuario;
 });
+
+Local.belongsTo(Usuario);
+Avaliacao.belongsTo(Usuario);
 
 module.exports = Usuario;
