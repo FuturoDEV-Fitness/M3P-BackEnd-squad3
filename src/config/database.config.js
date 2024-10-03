002978e04d811ad57b00adc8e300c8be16a907e0
Thaís <thaisefag@gmail.com>
2024-10-03 02:32:12 -0300

    draw

--- src/config/database.config.js
+++ src/config/database.config.js
@@ -2,8 +2,8 @@ const { config } = require("dotenv");
 config();
 
 module.exports = {
-  dialect: process.env.DB_DIALECT || "postgres",
-  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  host: process.env.DB_HOST || "postgres",
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
