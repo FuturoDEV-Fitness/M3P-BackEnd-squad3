d679d0299baf4a658f290507b862b883b8af7fbb
Thaís <thaisefag@gmail.com>
2024-10-07 22:08:20 -0300

    deploy3

--- src/database/migrations/20241007232148-alterar-locais.js
+++ src/database/migrations/20241007232148-alterar-locais.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.changeColumn('locais', 'itens_checkbox', {
+    await queryInterface.changeColumn("locais", "itens_checkbox", {
       type: Sequelize.STRING(),
       allowNull: false,
     });
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.changeColumn('locais', 'itens_checkbox', {
-      type: Sequelize.STRING(), 
+    await queryInterface.changeColumn("locais", "itens_checkbox", {
+      type: Sequelize.STRING(),
       allowNull: false,
     });
   },
